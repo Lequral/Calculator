7f9002b554784fe57aecae0695b8d2aaff91db9d
Lequral <monsieur.lol@yahoo.com>
2023-01-14 21:27:15 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/calculator/",
+  base: "",
   plugins: [svelte()],
 })
